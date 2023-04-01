ef4200831290fd980ae10b35fb03aa6beb7b8e4b
Diego-de <diego04dgs@gmail.com>
2023-04-01 15:36:58 -0300

    Segundo commit

--- src/main/java/com/project/login/autentication/SecurityConfig.java
+++ src/main/java/com/project/login/autentication/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.project.login.autentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -25,8 +26,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
         http.headers().frameOptions().sameOrigin();
         http.authorizeRequests()
-            .antMatchers("/users/authenticate").permitAll()
-            .antMatchers("/users/create").permitAll()
+            .antMatchers("/users/**").permitAll()
+            .antMatchers("/users/auth").permitAll()
+            .antMatchers(HttpMethod.POST, "/users/auth").permitAll()
+            .antMatchers("/users/signup").permitAll()
             .antMatchers("/h2-console/**").permitAll()
             .anyRequest().authenticated()
             .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
--- src/main/java/com/project/login/controller/UserController.java
+++ src/main/java/com/project/login/controller/UserController.java
@@ -1,87 +1,90 @@
-    package com.project.login.controller;
-    import javax.validation.Valid;
+package com.project.login.controller;
+import javax.validation.Valid;
 
-    import org.springframework.beans.factory.annotation.Autowired;
-    import org.springframework.http.ResponseEntity;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.project.login.autentication.AuthenticationRequest;
-    import com.project.login.autentication.AuthenticationResponse;
+import com.project.login.autentication.AuthenticationResponse;
 import com.project.login.autentication.JwtTokenUtil;
 import com.project.login.domain.User;
 import com.project.login.dto.LoginDto;
 import com.project.login.dto.UserDto;
-    import com.project.login.repository.UserRepository;
+import com.project.login.repository.UserRepository;
 import com.project.login.service.CustomUserDetail;
 import com.project.login.service.UserService;
 
-    import org.springframework.web.bind.annotation.CrossOrigin;
-    import org.springframework.web.bind.annotation.GetMapping;
-    import org.springframework.web.bind.annotation.PathVariable;
-    import org.springframework.web.bind.annotation.PostMapping;
-    import org.springframework.web.bind.annotation.RequestBody;
-    import org.springframework.web.bind.annotation.RequestMapping;
-    import org.springframework.web.bind.annotation.ResponseBody;
-    import org.springframework.web.bind.annotation.RestController;
 
 
+@RestController
+@RequestMapping("/users")
+@CrossOrigin("*")
+public class UserController {
 
-    @RestController
-    @RequestMapping("/users")
-    @CrossOrigin("http://localhost:3000/")
-    public class UserController {
 
-        @Autowired
-        UserRepository userRepository;
+    private static final Logger logger = Logger.getLogger(UserController.class.getName());
 
-        @Autowired
-        UserService Userservice;
+    @Autowired
+    UserRepository userRepository;
 
-        @Autowired
-        private AuthenticationManager authenticationManager;
+    @Autowired
+    UserService Userservice;
 
-        @Autowired
-        private JwtTokenUtil jwtTokenUtil;
-            
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
-        @GetMapping("/{id}")
-        @ResponseBody
-        public ResponseEntity<User> getUserById(@PathVariable Long id) {
-            return userRepository.findById(id)
-                    .map(user -> ResponseEntity.ok().body(user))
-                    .orElse(ResponseEntity.notFound().build());
-        }
-
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
         
-        @PostMapping("/signup")
-        public ResponseEntity<UserDto> createUser(@RequestBody @Valid UserDto userDto) {
 
-            User user = new User();
-            user.setEmail(userDto.getEmail());
-            user.setPassword(userDto.getPassword());
+    @GetMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(user -> ResponseEntity.ok().body(user))
+                .orElse(ResponseEntity.notFound().build());
+    }
 
-            Userservice.createUser(user);
+    
+    @PostMapping("/signup")
+    public ResponseEntity<UserDto> createUser(@RequestBody @Valid UserDto userDto) {
 
-            return ResponseEntity.ok().body(new UserDto(user));
-        }
+        User user = new User();
+        user.setEmail(userDto.getEmail());
+        user.setPassword(userDto.getPassword());
+
+        Userservice.createUser(user);
+
+        return ResponseEntity.ok().body(new UserDto(user));
+    }
 
 
-        
-        @PostMapping("/authenticate")
-        public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody LoginDto loginDto) throws Exception {
-            try {
-                authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword()));
-            } catch (BadCredentialsException e) {
-                throw new Exception("Invalid username or password", e);
-            }
-        
-            final CustomUserDetail userDetails = (CustomUserDetail) Userservice.loadUserByUsername(loginDto.getEmail());
-            final String jwt = jwtTokenUtil.generateToken(userDetails);
-        
-            return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    
+    @PostMapping("/auth")
+    public ResponseEntity<?> authenticate(@RequestBody LoginDto loginDto) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword()));
+        } catch (BadCredentialsException e) {
+            throw new Exception("Invalid username or password", e);
         }
     
+        final CustomUserDetail userDetails = (CustomUserDetail) Userservice.loadUserByUsername(loginDto.getEmail());
+        final String jwt = jwtTokenUtil.generateToken(userDetails);
 
+        AuthenticationResponse response = new AuthenticationResponse(jwt);
+    
+        return ResponseEntity.ok().body(response);
     }
+
+}
